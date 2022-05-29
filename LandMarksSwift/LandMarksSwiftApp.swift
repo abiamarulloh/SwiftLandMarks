c6b16f7967515841c30577d892d486e0cc63d79a
abi amarulloh <abi@appinzpire.net>
2022-05-30 06:36:35 +0700

    Initial Commit

---
+++ LandMarksSwift/LandMarksSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksSwiftApp.swift
+//  LandMarksSwift
+//
+//  Created by abi amarulloh on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
